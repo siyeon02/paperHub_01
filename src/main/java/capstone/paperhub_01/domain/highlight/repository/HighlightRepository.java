449e0b0d64da684f63077f9a25c8723f10988ba3
siyeon02 <chrysapark@cau.ac.kr>
2025-09-29 21:44:54 +0900

    highlight 레포지토리 생성

---
+++ src/main/java/capstone/paperhub_01/domain/highlight/repository/HighlightRepository.java
@@ -0,0 +1,12 @@
+package capstone.paperhub_01.domain.highlight.repository;
+
+import capstone.paperhub_01.domain.highlight.Highlight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface HighlightRepository extends JpaRepository<Highlight, Long> {
+    List<Highlight> findByPaperSha256AndPage(String sha256, Integer page);
+    List<Highlight> findByAnchorIdIn(Collection<Long> anchorIds);
+}
