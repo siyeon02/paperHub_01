27b634858b6925e32b757fcb7293618747986b73
siyeon02 <chrysapark@naver.com>
2025-10-10 15:30:06 +0900

    라이브러리 엔티티

---
+++ src/main/java/capstone/paperhub_01/domain/library/Library.java
@@ -0,0 +1,4 @@
+package capstone.paperhub_01.domain.library;
+
+public class Library {
+}
---
+++ src/main/java/capstone/paperhub_01/domain/library/repository/LibraryRepository.java
@@ -0,0 +1,4 @@
+package capstone.paperhub_01.domain.library.repository;
+
+public class LibraryRepository {
+}
