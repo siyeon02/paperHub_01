3c3cfd0a30896a032c3865317c2655f54687d732
siyeon02 <chrysapark@naver.com>
2025-10-12 00:40:39 +0900

    컬렉션 다건 조회/단건 조회할 때 paperinfo같이 보내는 걸로 수정 완료

--- src/main/java/capstone/paperhub_01/domain/collection/repository/CollectionPaperRepository.java
+++ src/main/java/capstone/paperhub_01/domain/collection/repository/CollectionPaperRepository.java
@@ -15,24 +15,55 @@ import java.util.Optional;
 public interface CollectionPaperRepository extends JpaRepository<CollectionPaper, Long> {
     Optional<CollectionPaper> findByPaperId(Long paperId);
 
-    @Query("""
-               select cp from CollectionPaper cp
-               join fetch cp.paper p
-               where cp.member.id = :memberId
-                 and cp.status = :status
-            """)
+//    @Query("""
+//               select cp from CollectionPaper cp
+//               join fetch cp.paper p
+//               where cp.member.id = :memberId
+//                 and cp.status = :status
+//            """)
+//    Page<CollectionPaper> searchByMemberAndStatus(@Param("memberId") Long memberId,
+//                                                  @Param("status") ReadingStatus status,
+//                                                  Pageable pageable);
+
+    @Query(
+            value = """
+        select cp
+        from CollectionPaper cp
+          join fetch cp.paper p
+          left join fetch p.paperInfo i
+        where cp.member.id = :memberId
+          and (:rs is null or cp.status = :rs)
+        order by cp.updatedAt desc
+      """,
+            countQuery = """
+        select count(cp)
+        from CollectionPaper cp
+        where cp.member.id = :memberId
+          and (:rs is null or cp.status = :rs)
+      """
+    )
     Page<CollectionPaper> searchByMemberAndStatus(@Param("memberId") Long memberId,
-                                                  @Param("status") ReadingStatus status,
+                                                  @Param("rs") ReadingStatus rs,
                                                   Pageable pageable);
 
-
     @Query("""
-        select cp from CollectionPaper cp
-        join fetch cp.paper p
-        where cp.id = :id
-          and cp.member.id = :memberId
-    """)
-    Optional<CollectionPaper> findInfoByIdAndMember(Long id, Long memberId);
+  select cp
+  from CollectionPaper cp
+    left join fetch cp.paper p
+    left join fetch p.paperInfo i
+  where cp.id = :id
+    and cp.member.id = :memberId
+""")
+    Optional<CollectionPaper> findInfoByIdAndMember(@Param("id") Long id,
+                                                    @Param("memberId") Long memberId);
+
+//    @Query("""
+//        select cp from CollectionPaper cp
+//        join fetch cp.paper p
+//        where cp.id = :id
+//          and cp.member.id = :memberId
+//    """)
+//    Optional<CollectionPaper> findInfoByIdAndMember(Long id, Long memberId);
 
     @Query("""
         select cp.status as status, count(cp) as count
