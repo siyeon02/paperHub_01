519d4b443ccc2b12b5d25c2e8201e5b551573781
siyeon02 <chrysapark@naver.com>
2025-10-12 00:40:39 +0900

    컬렉션 다건 조회/단건 조회할 때 paperinfo같이 보내는 걸로 수정 완료

--- src/main/java/capstone/paperhub_01/controller/collection/response/CollectionPaperInfo.java
+++ src/main/java/capstone/paperhub_01/controller/collection/response/CollectionPaperInfo.java
@@ -3,6 +3,7 @@ package capstone.paperhub_01.controller.collection.response;
 import capstone.paperhub_01.domain.collection.ReadingStatus;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 
 @Data
@@ -13,4 +14,16 @@ public class CollectionPaperInfo {
     private OffsetDateTime lastOpenedAt;
     private OffsetDateTime addedAt;
     private OffsetDateTime updatedAt;
+
+    //paperInfo 정보
+    private String title;
+    private String arxivId;
+    private String abstractText;
+    private String primaryCategory;
+    private String pdfUrl;
+
+    // 필요 시 화면에서 쓰면 채우고, 아니면 null 유지
+    private String authorsJson;      // 예: ["A B","C D"]
+    private String categoriesJson;   // 예: ["cs.CL","cs.AI"]
+    private LocalDate publishedDate; // arXiv 등 공개일
 }
